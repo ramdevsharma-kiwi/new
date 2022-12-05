059c5d0db9258913498411a5810a9d2beae1cee6
ramdevsharma-kiwi <ram.sharma@kiwitech.com>
2022-12-05 18:01:43 +0530

    add files

---
+++ firstchallenge/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// fs.writeFileSync("firstchallenge/read.txt", "Hy!! Welcome to kiwitech ...");
+
+// fs.appendFileSync("firstchallenge/read.txt","you are new here i will be always ther for u.....");
+
+//  const message =fs.readFileSync("firstchallenge/read.txt" ,"utf-8" );
+
+// console.log(message);
+
+
+// fs.renameSync("firstchallenge/read.txt","firstchallenge/readwrite.txt");
+// fs.unlinkSync("firstchallenge/readwrite.txt");
+
+
+
